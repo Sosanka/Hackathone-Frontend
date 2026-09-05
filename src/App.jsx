280a7a87358597d87e7ec84d5f4fa2c040f22127
Sosanka <santumoni03@gmail.com>
2026-09-05 20:28:17 +0530

    refactor: clean up imports and streamline route definitions in App component

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,9 @@ import Dashboard from "./pages/user/Dashboard";
 import MyListings from "./pages/user/MyListings";
 import LiveStocks from "./pages/user/LiveStocks";
 
-import Dashboard from "./pages/user/Dashboard";
-import MyListings from "./pages/user/MyListings";
-import LiveStocks from "./pages/user/LiveStocks";
 import Login from "./pages/Login";
 import SellerRegister from "./pages/SellerRegister";
 import Home from "./pages/Home";
-import UserRegister from "./pages/user/auth/UserRegister";
 
 import ProfileUpdatePage from "./pages/ProfileUpdatePage";
 import Onboarding from "./pages/user/Onboarding";
@@ -26,41 +22,28 @@ function App() {
   return (
     <Router>
       <Routes>
-
         {/* Default */}
-        <Route
-          path="/"
-          element={<Navigate to="/dashboard" replace />}
-        />
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
 
         {/* Authentication */}
-        <Route path="/" element={<Navigate to="/dashboard" replace />} />
-        {/* Login */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<SellerRegister />} />
-        <Route
-          path="/user/register"
-          element={<UserRegister />}
-        />
+        <Route path="/user/register" element={<UserRegister />} />
 
         {/* Onboarding & Profile */}
-        <Route
-          path="/onboarding"
-          element={<Onboarding />}
-        />
+        <Route path="/onboarding" element={<Onboarding />} />
 
-        <Route
-          path="/profile-update"
-          element={<ProfileUpdatePage />}
-        />
+        <Route path="/profile-update" element={<ProfileUpdatePage />} />
 
         {/* Home */}
         <Route path="/home" element={<Home />} />
 
+        {/* Seller Portal */}
         <Route path="/dashboard" element={<Dashboard />} />
+
         <Route path="/listings" element={<MyListings />} />
+
         <Route path="/stocks" element={<LiveStocks />} />
-        <Route path="/user/register" element={<UserRegister />} />
       </Routes>
     </Router>
   );
